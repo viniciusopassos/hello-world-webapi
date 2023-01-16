6b0fe753573d9030e60d0b862df758ba3aff55b4
viniciusopassos <viniciusopassos@hotmail.com>
2023-01-16 16:45:19 -0300

    deploy hello-world-cSharp

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello_world_webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloWorldController
+{
+    [HttpGet]
+    [Route("1")]
+    public string Get() => "Hello World!";
+    
+    [HttpGet]
+    [Route("2")]
+    public string  GetHelloTrybe() => "Hello Trybe!";
+}
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
